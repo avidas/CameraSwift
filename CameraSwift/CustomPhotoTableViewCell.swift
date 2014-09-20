a9542901142f2d500e8a04c760e95bbbe6fdb614
Avi Das <adas@colgate.edu>
2014-09-20 08:53:33 -0500

    Add Custom Photo Table View Cell

---
+++ CameraSwift/CustomPhotoTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPhotoTableViewCell.swift
+//  CameraSwift
+//
+//  Created by Das, Ananya on 9/19/14.
+//  Copyright (c) 2014 Das, Ananya. All rights reserved.
+//
+
+import UIKit
+
+class CustomPhotoTableViewCell: UITableViewCell {
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var title: UILabel!
+    @IBOutlet var titleView: UIView!
+    
+}
